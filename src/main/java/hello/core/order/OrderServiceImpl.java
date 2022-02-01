cf4a69c4a8c5f3e634f27f68b60f2d75334ed1e7
ash2748 <max3535@naver.com>
2022-02-01 11:59:36 +0900

    할인정책 전환 및 테스트

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -13,9 +13,9 @@ public class OrderServiceImpl implements OrderService{
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         //OrderService는 Member, Discount와 관련된 기능을 모른다(SRP(단일책임원칙) 잘 지켜진 설계)
-        Member member = memberRepository.findById(memberId);
-        int discountPrice = discountPolicy.discount(member, itemPrice);
+        Member member = memberRepository.findById(memberId); //회원 조회
+        int discountPrice = discountPolicy.discount(member, itemPrice); //할인 적용
 
-        return new Order(memberId, itemName, itemPrice, discountPrice);
+        return new Order(memberId, itemName, itemPrice, discountPrice); //주문 결과 반환
     }
 }
